c3862590213ccbded82f83a099f2cf32b2b198f1
Eboxsl <Eboxsl@hotmail.com>
2024-10-24 16:12:58 +0530

    Update robo.js

--- robo.js
+++ robo.js
@@ -1,7 +1,8 @@
-{"team1":"iron"
-"team2":"iron"
-"team3":"iron"
-"team4":"iron"
-"team5":"iron"
-"team6":"iron"
+{
+  "team1": "iron",
+  "team2": "iron",
+  "team3": "iron",
+  "team4": "iron",
+  "team5": "iron",
+  "team6": "iron"
 }

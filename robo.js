79b2f9dc7fe6ff6ffd9eddacb67f1542750aaef9
Eboxsl <Eboxsl@hotmail.com>
2024-11-28 19:01:23 +0530

    Update robo.js

--- robo.js
+++ robo.js
@@ -1,8 +1,54 @@
 {
   "team1": "🥉 Bronze 50〽️",
+//task 1
+    //Disindu Senumitha
+    //Aritha Savain
+    //V.G.M.R.Vindinu
+    //vithushaka
+    //T.H.Chirath Sihilel Hewage 
+
+//task2
+//M.G.Disindu Senumitha -9
+//Vithushka -7
+//T.H.Chirath Sihilel Hewage -8
+//vindinu-8
+
+    
   "team2": "⚙️ Iron 40",
+//task1
+//P.D.Thamidu Manula
+//beta-Chenuka Sadew
+//beta-thisanga
+//
+
+
+    //task2
+//Ayod Methnula-8
+//chenuka sadew-6
+
+
+
+
+    
   "team3": "⚙️ Iron",
+
+    //task1
+
+    //task2
+//T.H Chirath hewage
+
+
+
   "team4": "⚙️ Iron 10",
+
+    //task1
+
+    //task2
+  //  W .N. Senuk Dunujitha
+
+
+
+
   "team5": "⚙️ Iron",
   "team6": "⚙️ Iron"
 }
